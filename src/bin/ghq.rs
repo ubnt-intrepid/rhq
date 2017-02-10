b7b2bd91a4828aef0376911f061b427238137c4f
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-02-11 01:57:17 +0900

    wip: load config

--- src/bin/ghq.rs
+++ src/bin/ghq.rs
@@ -2,9 +2,9 @@ extern crate rhq;
 extern crate env_logger;
 
 fn main() {
-    env_logger::init().unwrap();
-    if let Err(message) = rhq::run() {
-        println!("failed with: {}", message);
-        std::process::exit(1);
-    }
+  env_logger::init().unwrap();
+  if let Err(message) = rhq::run() {
+    println!("failed with: {}", message);
+    std::process::exit(1);
+  }
 }
