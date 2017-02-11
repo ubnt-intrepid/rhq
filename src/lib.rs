2e5df99c1a1360b04a15d17d2b2b6ab6ab1431ca
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-02-12 03:08:17 +0900

    wip:

--- src/lib.rs
+++ src/lib.rs
@@ -29,10 +29,18 @@ pub mod vcs;
 pub use errors::*;
 pub use config::Config;
 
-pub fn clone_repository(query: &str, args: Vec<&str>) -> errors::Result<()> {
-  println!("{}, ({:?})", query, args);
-  println!("{:?}", remote::build_url(query));
+pub fn make_local_path_from_url(url: &url::Url) -> errors::Result<::std::path::PathBuf> {
+  let mut path = url.host_str().map(ToOwned::to_owned).ok_or("url.host() is empty".to_owned())?;
+  path += url.path().trim_right_matches(".git");
+  Ok(path.into())
+}
 
+pub fn clone_repository(query: &str, args: Vec<&str>) -> errors::Result<()> {
+  let url = remote::build_url(query)?;
+  let path = make_local_path_from_url(&url)?;
+  // TODO: check if local repository has already cloned at `path`.
+  println!("[debug] clone: {:?} => {:?} (args = {:?})", url, path, args);
+  // TODO: perform `git clone`.
   Ok(())
 }
 
