258c8c0b9419ea536ca344ac0c0e5789bcf47b93
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-02-12 22:22:49 +0900

    add make_package.bat

---
+++ ci/make_package.bat
@@ -0,0 +1,10 @@
+@echo on
+IF NOT DEFINED PKGNAME set PKGNAME=rhq
+IF NOT DEFINED HOST set HOST=arm-linux-androideabi
+
+cargo build --release --target=%HOST%
+
+if     exist "%PKGNAME%-%HOST%\" del   %PKGNAME%-%HOST%
+if not exist "%PKGNAME%-%HOST%\" mkdir %PKGNAME%-%HOST%
+
+copy target\%HOST%\release\%PKGNAME%.exe   "%PKGNAME%-%HOST%\"
