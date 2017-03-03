8622910af03b84ed7771e726f3d2343f17166ab7
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-03-04 04:56:11 +0900

    wip:

--- ci/build.sh
+++ ci/build.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 script_dir="$(cd $(dirname $BASH_SOURCE); pwd)"
 skip_test="${1:-}"
 
-$script_root/cargo.sh build
+$script_dir/cargo.sh build
 
 if [[ -z $skip_test ]]; then
-  $script_root/cargo.sh test
+  $script_dir/cargo.sh test
 fi
