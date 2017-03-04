e6cc3ddefa1268643aedf03a73d04da9efea0abd
Yusuke Sasaki <ubnt-intrepid@users.noreply.github.com>
2017-03-04 17:31:27 +0900

    Use docker for deployment (#4)

--- ci/cargo.sh
+++ ci/cargo.sh
@@ -2,18 +2,10 @@
 
 set -euo pipefail
 
-if [[ -z "$TARGET" ]]; then
-  echo "\$TARGET is empty."
-  exit 1
-fi
-
-case $TARGET in
-  *-unknown-linux-musl|*-linux-androideabi|*-linux-android)
-    echo "[Use docker container]"
-    docker exec -it rust-container cargo "$@"
+case `uname -s` in
+  Linux)
+    docker exec -it "rust" cargo "$@"
     ;;
   *)
-    echo "[Use standard toolchain]"
     cargo "$@"
-    ;;
 esac
--- ci/install.sh
+++ ci/install.sh
@@ -1,71 +1,29 @@
 #!/bin/bash
 
 set -euo pipefail
-set -x
 
 script_dir="$(cd $(dirname $BASH_SOURCE); pwd)"
 
-target="$1"
-toolchain="$2"
+target="${1:-x86_64-unknown-linux-gnu}"
+toolchain="${2:-stable}"
+image_name="rust-${target}-${toolchain}"
+container_name=rust
 
-setup_rustup_docker() {
-  docker build -t "$1" "${@:2}" $script_dir/docker/"$1"
-
-  docker rm -f rust-container || true
-  docker run --name rust-container \
-    -d -it \
-    -v "$(pwd)":/home/rust/src \
-    -w /home/rust/src \
-    --privileged \
-    "$1"
-
-  docker exec -it rust-container rustup update
-  docker exec -it rust-container rustup default "$toolchain"
-  case $target in
-  i686-unknown-linux-musl|*-linux-androideabi|*-linux-android)
-    docker exec -it rust-container rustup target install "$target"
+case `uname -s` in
+  Linux)
+    $script_dir/docker/start_container.sh "$target" "$toolchain" "$image_name" "$container_name"
+    # check installation
+    docker exec -it "$container_name" rustup --version
+    docker exec -it "$container_name" rustup target list | grep 'default\|installed'
+    docker exec -it "$container_name" cargo --version
     ;;
-  esac
-}
 
-
-setup_rustup() {
-  case $target in
-  i686-unknown-linux-gnu|*-unknown-freebsd)
-    curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain "$toolchain"
+  Darwin)
+    curl -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain "$toolchain" --default-host "$target"
+    source $HOME/.cargo/env
+    # check installation
+    rustup --version
+    rustup target list | grep 'default\|installed'
+    cargo --version
     ;;
-  *)
-    curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain "$toolchain" --default-host "$target"
-    ;;
-  esac
-  case $target in
-  i686-unknown-linux-gnu|*-unknown-freebsd)
-    $HOME/.cargo/bin/rustup target add "$target" ;;
-  esac
-}
-
-
-case $target in
-*-unknown-linux-musl)
-  setup_rustup_docker "rust-musl-builder"
-  ;;
-
-*-linux-androideabi|*-linux-android)
-  setup_rustup_docker "rust-android-builder" \
-    --build-arg TARGET=$TARGET \
-    --build-arg ARCH=$ARCH \
-    --build-arg API=$API
-  ;;
-
-*)
-  setup_rustup
-  ;;
 esac
-
-# check installation
-source $HOME/.cargo/env || true
-export TARGET=$target
-$script_dir/rustup.sh --version
-$script_dir/rustup.sh target list | grep 'default\|installed'
-$script_dir/cargo.sh --version
-$script_dir/rustup.sh run "$toolchain" rustc --version
