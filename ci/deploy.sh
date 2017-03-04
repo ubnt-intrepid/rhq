93675dd5183b715823eb88b5bf0678ea9c577bdd
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-03-04 15:25:50 +0900

    wip:

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,11 +5,8 @@ set -euo pipefail
 script_dir="$(cd $(dirname $BASH_SOURCE); pwd)"
 pkgname="$1"
 
-$script_dir/cargo.sh build --release
-
 rm -rf ./"${pkgname}"
-mkdir -p ./"${pkgname}"
-cp ./target/release/rhq ./"${pkgname}"/
+$script_dir/cargo.sh install --root ./"${pkgname}"
 
 cd "${pkgname}"
 tar -zcf ../"${pkgname}.tar.gz" ./*
--- ci/install.sh
+++ ci/install.sh
@@ -6,12 +6,12 @@ script_dir="$(cd $(dirname $BASH_SOURCE); pwd)"
 
 target="${1:-x86_64-unknown-linux-gnu}"
 toolchain="${2:-stable}"
-image_name="rust-$target"
+image_name="rust-${target}-${toolchain}"
 container_name=rust
 
 case `uname -s` in
   Linux)
-    $script_dir/../docker/start_container.sh "$target" "$toolchain" "$image_name" "$container_name"
+    $script_dir/docker/start_container.sh "$target" "$toolchain" "$image_name" "$container_name"
     # check installation
     docker exec -it "$container_name" rustup --version
     docker exec -it "$container_name" rustup target list | grep 'default\|installed'
