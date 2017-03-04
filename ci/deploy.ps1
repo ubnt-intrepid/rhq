d0eeb90c27312e920046993071df4f5608fa2f7e
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-03-04 16:54:14 +0900

    wip:

--- ci/deploy.ps1
+++ ci/deploy.ps1
@@ -3,7 +3,7 @@ param(
   [string]$pkgname
 )
 
-Remove-Item -Recurse -Force ".\$($pkgname)"
+Remove-Item -Recurse -Force ".\$($pkgname)" -ErrorAction SilentlyContinue
 cargo install --root ".\$($pkgname)"
 
 7z a "$($pkgname).zip" ".\$($pkgname)\"
