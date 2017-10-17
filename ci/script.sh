b7585ee8c921272ba6d9beadc6c1fd7ca77ac4c7
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-10-17 19:17:42 +0900

    chore(ci): fix scripts

--- ci/script.sh
+++ ci/script.sh
@@ -6,13 +6,13 @@ set -ex
 
 # TODO This is the "test phase", tweak it as you see fit
 main() {
-    cross build --target $TARGET --release
+    cross build --all --target $TARGET --release
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
     fi
 
-    cross test --target $TARGET --release
+    cross test --all --target $TARGET --release
 }
 
 # we don't run the "test phase" when doing deploys
