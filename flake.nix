f3873aa51fbd95b0bd4768b6bd43af9647057d15
Yusuke Sasaki <ubnt.intrepid@gmail.com>
2026-02-07 16:05:16 +0900

    setup rust-overlay

--- flake.nix
+++ flake.nix
@@ -2,30 +2,36 @@
   description = "A repository management tool";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgsFor = system: nixpkgs.legacyPackages.${system};
-    in
-    {
-      packages = forAllSystems (system: {
-        default = (pkgsFor system).rustPlatform.buildRustPackage {
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rust = pkgs.rustPlatform;
+      in
+      {
+        packages.default = rust.buildRustPackage {
           pname = "rhq";
           version = "0.4.0-dev";
           src = ./.;
           cargoLock.lockFile = ./Cargo.lock;
         };
-      });
-      devShells = forAllSystems (system: {
-        default = (pkgsFor system).mkShell {
-          buildInputs = with (pkgsFor system); [
-            cargo
-            rustc
-            rust-analyzer
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            git # Needed for cargo-init and other Git operations within the shell.
+            # pkg-config openssl ...
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-analyzer" "clippy" "rustfmt" ];
+            })
           ];
+          RUST_SRC_PATH = rust.rustLibSrc; # Ensure rust-analyzer can find the source code.
         };
-      });
-    };
+      }
+    );
 }
